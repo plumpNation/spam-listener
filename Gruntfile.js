42f1997ac64780e010657e60c57dc296bcd73b93
freshteapot <chris@freshteapot.net>
2013-08-19 21:41:59 +0000

    Made the site run on my machine.
    Really cool stuff!

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,53 @@
+'use strict';
+var path = require('path');
+
+var folderMount = function folderMount(connect, point) {
+  return connect.static(path.resolve(point));
+};
+
+
 module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        connect: {
+          main: {
+            options: {
+                hostname: '0.0.0.0',
+              port: 9003,
+              middleware: function(connect, options) {
+                return [folderMount(connect, options.base)]
+              }
+            }
+          }
+        },
+        watch: {
+          main: {
+            options: {
+                livereload: 35730
+            },
+            files: ['p-elements/**/*','scripts/**/*','index.html'],
+            tasks: ['jshint']
+          }
+        },
+        jshint: {
+          options: {
+            curly: true,
+            eqeqeq: true,
+            immed: true,
+            latedef: true,
+            newcap: true,
+            noarg: true,
+            sub: true,
+            undef: false,
+            boss: true,
+            eqnull: true,
+            browser: true,
+            smarttabs: true
+          },
+          files: ['scripts/**/*.js']
+        },
         less: {
             development: {
                 options: {
@@ -25,8 +70,12 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
 
     // Default task(s).
     grunt.registerTask('default', ['less']);
-
+    grunt.registerTask('server', ['jshint','connect', 'watch']);
 };
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 An attempt to capture the conversation in a team environment with the goal of reporting on the collected data.
+
+
+Setup:
+
+```
+npm install
+```
+
+Then
+
+```
+bower install polymer
+```
+
+Run:
+```
+grunt server
+```
+
+Then point your browser to your "ip address":9003
